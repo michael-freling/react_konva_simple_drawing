abc55acc08d30aeee0e5a3a455ac1843aba070d9
Michael <80940180+michael-freling@users.noreply.github.com>
2023-05-06 18:12:08 -0700

    add undo and redo feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,37 @@ import { Stage, Layer, Line, Text } from "react-konva";
 
 export default function Home() {
   const [tool, setTool] = React.useState("pen");
-  const [lines, setLines] = React.useState([]);
+  const [history, setHistory] = React.useState<
+    {
+      tool: string;
+      points: number[];
+    }[]
+  >([]);
   const isDrawing = React.useRef(false);
+  // undo/redo
+  const [historyStep, setHistoryStep] = React.useState(0);
+
+  // https://konvajs.org/docs/react/Undo-Redo.html
+  const handleUndo = () => {
+    if (historyStep === 0) {
+      return;
+    }
+    setHistoryStep(historyStep - 1);
+  };
+
+  const handleRedo = () => {
+    if (historyStep === history.length) {
+      return;
+    }
+    setHistoryStep(historyStep + 1);
+  };
 
   const handleMouseDown = (e) => {
+    // https://konvajs.org/docs/react/Free_Drawing.html
     isDrawing.current = true;
     const pos = e.target.getStage().getPointerPosition();
-    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+    setHistory([...history, { tool, points: [pos.x, pos.y] }]);
+    setHistoryStep(historyStep + 1);
   };
 
   const handleMouseMove = (e) => {
@@ -21,13 +45,13 @@ export default function Home() {
     }
     const stage = e.target.getStage();
     const point = stage.getPointerPosition();
-    let lastLine = lines[lines.length - 1];
+    let lastLine = history[history.length - 1];
     // add point
     lastLine.points = lastLine.points.concat([point.x, point.y]);
 
     // replace last
-    lines.splice(lines.length - 1, 1, lastLine);
-    setLines(lines.concat());
+    history.splice(history.length - 1, 1, lastLine);
+    setHistory(history.concat());
   };
 
   const handleMouseUp = () => {
@@ -36,15 +60,26 @@ export default function Home() {
 
   return (
     <div>
-      <select
-        value={tool}
-        onChange={(e) => {
-          setTool(e.target.value);
-        }}
-      >
-        <option value="pen">Pen</option>
-        <option value="eraser">Eraser</option>
-      </select>
+      <ul style={{ display: "inline-block" }}>
+        <li style={{ display: "inline" }}>
+          <select
+            value={tool}
+            onChange={(e) => {
+              setTool(e.target.value);
+            }}
+          >
+            <option value="pen">Pen</option>
+            <option value="eraser">Eraser</option>
+          </select>
+        </li>
+        <li style={{ display: "inline", padding: 2 }}>
+          <button onClick={handleUndo}>undo</button>
+        </li>
+        <li style={{ display: "inline", padding: 2 }}>
+          <button onClick={handleRedo}>redo</button>
+        </li>
+      </ul>
+
       <Stage
         width={window.innerWidth}
         height={window.innerHeight}
@@ -53,21 +88,24 @@ export default function Home() {
         onMouseup={handleMouseUp}
       >
         <Layer>
-          <Text text="Just start drawing" x={5} y={30} />
-          {lines.map((line, i) => (
-            <Line
-              key={i}
-              points={line.points}
-              stroke="#df4b26"
-              strokeWidth={5}
-              tension={0.5}
-              lineCap="round"
-              lineJoin="round"
-              globalCompositeOperation={
-                line.tool === "eraser" ? "destination-out" : "source-over"
-              }
-            />
-          ))}
+          {history
+            .filter((_, i) => {
+              return i < historyStep;
+            })
+            .map((line, i) => (
+              <Line
+                key={i}
+                points={line.points}
+                stroke="#df4b26"
+                strokeWidth={5}
+                tension={0.5}
+                lineCap="round"
+                lineJoin="round"
+                globalCompositeOperation={
+                  line.tool === "eraser" ? "destination-out" : "source-over"
+                }
+              />
+            ))}
         </Layer>
       </Stage>
     </div>
