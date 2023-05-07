f39e2ddb212250bad902997ce2e542051574b29c
Michael <80940180+michael-freling@users.noreply.github.com>
2023-05-06 17:29:53 -0700

    npm install react-konva konva

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+const webpack = require('webpack');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        // https://github.com/konvajs/react-konva/issues/102#issuecomment-308000612
+        config.plugins.push(new webpack.IgnorePlugin({
+            resourceRegExp: /canvas|jsdom/,
+            contextRegExp: /konva/,
+        }))
+        return config;
+    }
+}
 
 module.exports = nextConfig
