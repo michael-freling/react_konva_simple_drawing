80d38ef5b960dcdfc6c7c44a107160ecba0d4b76
Michael <80940180+michael-freling@users.noreply.github.com>
2023-05-07 20:17:58 -0700

    feature: add a fill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,10 +37,111 @@ const URLImage = ({ image, draggable, setImage }) => {
   );
 };
 
+// http://www.williammalone.com/articles/html5-canvas-javascript-paint-bucket-tool/
+function fill(canvas, startX, startY, color) {
+  const canvasContext = canvas.getContext("2d");
+  let imageData = canvasContext.getImageData(0, 0, canvas.width, canvas.height);
+  let newImageData = imageData;
+  imageData = imageData.data;
+
+  const fillRed = parseInt(color.substring(1, 3), 16);
+  const fillGreen = parseInt(color.substring(3, 5), 16);
+  const fillBlue = parseInt(color.substring(5, 7), 16);
+
+  const numberOfArrayPerPixel = 4;
+
+  const canvasImageIndex =
+    (canvas.width * startY + startX) * numberOfArrayPerPixel;
+  const originalRed = imageData[canvasImageIndex];
+  const originalGreen = imageData[canvasImageIndex + 1];
+  const originalBlue = imageData[canvasImageIndex + 2];
+  const originalAlpha = imageData[canvasImageIndex + 3];
+
+  console.debug({
+    imageData,
+    canvasImageIndex,
+    start: {
+      x: startX,
+      y: startY,
+      r: originalRed,
+      g: originalGreen,
+      b: originalBlue,
+    },
+    fillRed,
+    fillBlue,
+    fillGreen,
+    color,
+    width: canvas.width,
+    height: canvas.height,
+  });
+
+  let imageMap: {
+    [index: number]: {
+      [index: number]: number;
+    };
+  } = {};
+
+  function searchFill(startX: number, startY: number) {
+    const pixelStack: { y: number; x: number }[] = [{ y: startY, x: startX }];
+
+    while (pixelStack.length > 0) {
+      const { x, y } = pixelStack.pop()!;
+      if (y < 0) {
+        continue;
+      }
+      if (x < 0) {
+        continue;
+      }
+      if (x >= canvas.width) {
+        continue;
+      }
+      if (y >= canvas.height) {
+        continue;
+      }
+
+      const canvasImageIndex = (canvas.width * y + x) * numberOfArrayPerPixel;
+      const red = imageData[canvasImageIndex];
+      const green = imageData[canvasImageIndex + 1];
+      const blue = imageData[canvasImageIndex + 2];
+      // TODO: No way to store an alpha yet
+      const alpha = imageData[canvasImageIndex + 3];
+
+      if (imageMap[y] == null) {
+        imageMap[y] = {};
+      }
+      if (imageMap[y][x] === 1) {
+        // already searched
+        continue;
+      }
+      imageMap[y][x] = 1;
+
+      if (
+        red === originalRed &&
+        blue === originalBlue &&
+        green === originalGreen
+      ) {
+        newImageData.data[canvasImageIndex] = fillRed;
+        newImageData.data[canvasImageIndex + 1] = fillGreen;
+        newImageData.data[canvasImageIndex + 2] = fillBlue;
+        newImageData.data[canvasImageIndex + 3] = 255; // alpha
+
+        pixelStack.push({ x: x - 1, y });
+        pixelStack.push({ x: x + 1, y });
+        pixelStack.push({ x, y: y - 1 });
+        pixelStack.push({ x, y: y + 1 });
+      }
+    }
+  }
+
+  searchFill(startX, startY);
+  return newImageData;
+}
+
 enum Tool {
   Pen = "pen",
   Eraser = "eraser",
   Move = "move",
+  Fill = "fill",
 }
 
 enum Operation {
@@ -48,9 +149,12 @@ enum Operation {
   Erase = "erase",
   Drag = "drag",
   Import = "import",
+  Fill = "fill",
 }
 
 export default function Home() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
   const [tool, setTool] = React.useState<Tool>(Tool.Pen);
   const [history, setHistory] = React.useState<
     {
@@ -71,15 +175,15 @@ export default function Home() {
   // undo/redo: https://konvajs.org/docs/react/Undo-Redo.html
   const [historyStep, setHistoryStep] = React.useState(0);
   const stageRef = React.useRef(null);
+  const layerRef = React.useRef(null);
 
   // Import
   // https://konvajs.org/docs/react/Images.html
   // https://stackoverflow.com/questions/37457128/react-open-file-browser-on-click-a-div
   const inputFile = React.useRef<HTMLInputElement | null>(null);
-  const [images, setImages] = React.useState([]);
 
   // color
-  const [color, setColor] = React.useState([]);
+  const [color, setColor] = React.useState("#000000");
 
   const addToHistory = (newOperation) => {
     let newHistory = history;
@@ -108,10 +212,36 @@ export default function Home() {
   };
 
   const handleMouseDown = (e) => {
+    if (tool === Tool.Fill) {
+      const pointerPos = stageRef.current.getPointerPosition();
+
+      // Use FloodFill to fill the region around the clicked point
+      // const canvas = layerRef.current.getCanvas();
+      const canvas = stageRef.current.toCanvas();
+
+      const imageData = fill(canvas, pointerPos.x, pointerPos.y, color);
+
+      // Create a new canvas element with the filled imageData
+      const filledCanvas = document.createElement("canvas");
+      filledCanvas.width = canvas.width;
+      filledCanvas.height = canvas.height;
+      filledCanvas.getContext("2d")!.putImageData(imageData, 0, 0);
+
+      const filledImage = new window.Image();
+      filledImage.src = filledCanvas.toDataURL();
+      filledImage.onload = () => {
+        addToHistory({
+          operation: Operation.Fill,
+          tool,
+          image: filledImage,
+        });
+      };
+      // canvas.floodFill(pointerPos.x, pointerPos.y, [], color);
+      return;
+    }
     if (tool !== Tool.Pen && tool !== Tool.Eraser) {
       return;
     }
-
     // https://konvajs.org/docs/react/Free_Drawing.html
     isDrawing.current = true;
     const pos = e.target.getStage().getPointerPosition();
@@ -177,7 +307,6 @@ export default function Home() {
 
       const reader = new FileReader();
       reader.onload = (e) => {
-        console.log(e.target.result);
         addToHistory({
           operation: Operation.Import,
           image: {
@@ -195,11 +324,18 @@ export default function Home() {
 
   // https://codesandbox.io/s/jq5hm?file=/index.js:418-584
   const handleChangeColor = (event) => {
+    let colorCode = event.target.value;
     setColor(event.target.value);
   };
 
   // let imageRendered: [id: string]: bool = {}
   let imageRendered: Object = {};
+
+  let canvasContext = null;
+  if (layerRef.current != null) {
+    const canvas = layerRef.current.getCanvas();
+    canvasContext = canvas.getContext("2d");
+  }
 
   return (
     <div>
@@ -209,7 +345,6 @@ export default function Home() {
         ref={inputFile}
         style={{ display: "none" }}
         onChange={(e) => {
-          console.log(e);
           handleFiles(e.target.files);
         }}
       />
@@ -229,6 +364,7 @@ export default function Home() {
             }}
           >
             <option value={Tool.Pen}>Pen</option>
+            <option value={Tool.Fill}>Fill</option>
             <option value={Tool.Eraser}>Eraser</option>
             <option value={Tool.Move}>Move</option>
           </select>
@@ -245,14 +381,14 @@ export default function Home() {
       </ul>
 
       <Stage
-        width={window.innerWidth}
-        height={window.innerHeight}
+        width={width}
+        height={height}
         onMouseDown={handleMouseDown}
         onMousemove={handleMouseMove}
         onMouseup={handleMouseUp}
         ref={stageRef}
       >
-        <Layer>
+        <Layer ref={layerRef}>
           {history
             .filter((historyOperation, i) => {
               if (i >= historyStep) {
@@ -260,11 +396,14 @@ export default function Home() {
               }
               return (
                 historyOperation.operation === Operation.FreeDraw ||
-                historyOperation.operation === Operation.Erase
+                historyOperation.operation === Operation.Erase ||
+                historyOperation.operation === Operation.Fill
               );
             })
             .map((historyOperation, i) => {
               switch (historyOperation.tool) {
+                case Tool.Fill:
+                  return <Image key={i} image={historyOperation.image} />;
                 case Tool.Pen:
                 case Tool.Eraser:
                   return (
@@ -313,7 +452,6 @@ export default function Home() {
               }
             }
             imageRendered[image.id] = true;
-            console.log(history);
 
             return (
               <Layer key={image.id}>
