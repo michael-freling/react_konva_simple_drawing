75419625a17ffd3489a43d94791a1f88b4afedd3
Michael <80940180+michael-freling@users.noreply.github.com>
2023-05-14 23:38:32 -0700

    refactor: add layer editing

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -6,11 +6,12 @@ enum LayerType {
   Vector = "vector",
 }
 
-type LayerProps = {
+type BaseLayerProps = {
   id: string;
   name: string;
   type: LayerType;
-  isCurrent: true;
+  isCurrent: boolean;
+  isSelected: boolean;
 };
 
 enum Tool {
@@ -23,9 +24,12 @@ type FreeDrawLine = {
   color: string;
   points: number[];
 };
-type VectorLayerProps = LayerProps & {
+type VectorLayerProps = BaseLayerProps & {
   lines: FreeDrawLine[];
 };
+type ImageLayerProps = BaseLayerProps;
+
+type LayerProps = VectorLayerProps | ImageLayerProps;
 
 function VectorLayer({ id, lines }: VectorLayerProps) {
   return (
@@ -49,19 +53,20 @@ function VectorLayer({ id, lines }: VectorLayerProps) {
 }
 
 interface Command {
+  run(): void;
   undo(): void;
-  redo(): void;
 }
 
 export default function App() {
   const [history, setHistory] = React.useState<Command[]>([]);
   const [historyIndex, setHistoryIndex] = React.useState(0);
-  const [layers, setLayers] = React.useState<VectorLayerProps[]>([
+  const [layers, setLayers] = React.useState<LayerProps[]>([
     {
       id: "layer-1",
       name: "Layer 1",
       type: LayerType.Vector,
       isCurrent: true,
+      isSelected: false,
       lines: [],
     },
   ]);
@@ -90,7 +95,7 @@ export default function App() {
       setLayers([...layers]);
     }
 
-    redo() {
+    run() {
       layers[this.layerIndex].lines = this.lines;
       setLayers([...layers]);
     }
@@ -157,8 +162,83 @@ export default function App() {
     }
 
     const command = history[historyIndex];
-    command.redo();
+    command.run();
     setHistoryIndex(historyIndex + 1);
+  };
+
+  class AddLayerCommand implements Command {
+    layers: LayerProps[];
+    newLayer: LayerProps;
+
+    constructor(layers: LayerProps[], newLayer: LayerProps) {
+      this.layers = layers.concat();
+      this.newLayer = newLayer;
+    }
+
+    undo() {
+      setLayers([...this.layers]);
+    }
+
+    run() {
+      setLayers([...this.layers, this.newLayer]);
+    }
+  }
+
+  const handleAddLayer = (layerType: LayerType) => {
+    const newLayer: LayerProps = {
+      id: "layer-" + (layers.length + 1),
+      name: "Layer " + (layers.length + 1),
+      type: layerType,
+      isCurrent: false,
+      isSelected: false,
+      lines: [],
+    };
+    const command = new AddLayerCommand(layers, newLayer);
+    addToHistory(command);
+    command.run();
+  };
+
+  class DeleteSelectedLayersCommand implements Command {
+    layers: LayerProps[];
+    newLayers: LayerProps[];
+
+    constructor(layers: LayerProps[], newLayers: LayerProps[]) {
+      this.layers = layers.concat();
+      this.newLayers = newLayers.concat();
+    }
+
+    undo() {
+      setLayers([...this.layers]);
+    }
+
+    run() {
+      if (
+        this.newLayers.filter((layer) => layer.id === currentLayer.id)
+          .length === 0
+      ) {
+        let newLayers = this.newLayers.concat();
+        newLayers[0].isCurrent = true;
+        setLayers([...newLayers]);
+      } else {
+        setLayers([...this.newLayers]);
+      }
+    }
+  }
+
+  const handleDeleteSelectedLayers = () => {
+    const selectedLayers = layers.filter((layer) => layer.isSelected);
+    if (selectedLayers.length === 0) {
+      return;
+    }
+
+    const newLayers = layers.filter((layer) => !layer.isSelected);
+    if (selectedLayers.length === 0) {
+      return;
+    }
+
+    const command = new DeleteSelectedLayersCommand(layers, newLayers);
+    addToHistory(command);
+    command.run();
   };
 
   return (
@@ -183,6 +263,57 @@ export default function App() {
         </li>
       </ul>
 
+      <ul style={{ display: "block" }}>
+        <li style={{ display: "inline-block" }}>
+          <button
+            onClick={() => {
+              handleAddLayer(LayerType.Vector);
+            }}
+          >
+            Add a vector layer
+          </button>
+        </li>
+        <li style={{ display: "inline-block" }}>
+          <button onClick={handleDeleteSelectedLayers}>
+            Delete select layers
+          </button>
+        </li>
+      </ul>
+
+      <ul style={{ padding: 2 }}>
+        {layers.map((layer, index) => {
+          return (
+            <li key={layer.id} style={{ padding: 2 }}>
+              <input
+                type="checkbox"
+                id={layer.id}
+                checked={layer.isSelected}
+                onChange={() => {
+                  layers.forEach((layer, j) => {
+                    if (index === j) {
+                      layers[index].isCurrent = true;
+                    } else {
+                      layers[j].isCurrent = false;
+                    }
+                  });
+                  layers[index].isSelected = !layers[index].isSelected;
+                  setLayers([...layers]);
+                }}
+              />
+              <label htmlFor={layer.id}>
+                {layer.id === currentLayer.id ? (
+                  <span style={{ backgroundColor: "orange" }}>
+                    {layer.name} ({layer.type})
+                  </span>
+                ) : (
+                  layer.name + " (" + layer.type + ")"
+                )}
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+
       <Stage
         width={window.innerWidth}
         height={window.innerHeight}
@@ -193,9 +324,17 @@ export default function App() {
       >
         <Layer>
           <Text text="Just start drawing" x={5} y={30} />
-          {layers.map((layer) => (
-            <VectorLayer key={layer.id} {...layer} />
-          ))}
+          {layers.map((layer) => {
+            switch (layer.type) {
+              case LayerType.Vector:
+                return (
+                  <VectorLayer
+                    key={layer.id}
+                    {...(layer as VectorLayerProps)}
+                  />
+                );
+            }
+          })}
         </Layer>
       </Stage>
     </div>
